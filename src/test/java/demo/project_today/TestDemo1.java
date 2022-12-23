194bdd719a39aa7b1a142a9a1f2a2afd19ded7a6
shilpa-bhadra <shilpa.bhadran@gmail.com>
2022-12-23 13:17:12 +0530

    second commit

--- src/test/java/demo/project_today/TestDemo1.java
+++ src/test/java/demo/project_today/TestDemo1.java
@@ -5,8 +5,9 @@ import org.testng.annotations.Test;
 public class TestDemo1 {
 	@Test
 	public void demo1() {
-		System.out.println("TestDemo1");
-
+		System.out.println("TestDemo1 is the changed code");
+		System.out.println(" changed code");
+		System.out.println(" code");
 	}
 	
 	
